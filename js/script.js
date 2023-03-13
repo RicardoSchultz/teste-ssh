cf742d1b9cdb1f26e23021882f15d70e238bf52b
Ricardo Schultz <schultz.ricardo@hotmail.com>
2023-03-13 14:36:53 -0300

    teste arquivo add

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var teste = "imagem1.png<br/>imagem2.png<br/>"
+teste.split('</br>');
+
+console.log(teste);
---
+++ js/script2.js
@@ -0,0 +1,5 @@
+const socialNetworks = ['youtube', 'facebook', 'twitter', 'instagran','youtube', 'facebook', 'twitter', 'instagran' ];
+
+socialNetworks;
+console.log(socialNetworks);
+
